6dad3ce597f9cc4031081f9daa7b6aa5d1c87ea1
farooq94 <farooq.0346@hotmail.com>
2017-02-11 02:38:56 +0500

    initial commit

--- FypSwiftUITests/SecondViewController.swift
+++ FypSwiftUITests/SecondViewController.swift
@@ -16,7 +16,7 @@ class SecondViewController: UIViewController {
         
         
 
-        // Do any additional  setup after loading the view.
+        // Do any additional     setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
