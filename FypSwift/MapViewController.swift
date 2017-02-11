270e900fb5a458875c6562ea3e0f2065a47b7868
MBP <mbp@mbps-macbook-pro.local>
2017-02-11 17:13:03 +0500

    Work on map

--- FypSwift/MapViewController.swift
+++ FypSwift/MapViewController.swift
@@ -9,7 +9,32 @@
 import UIKit
 import GoogleMaps
 
-class MapViewController: UIViewController {
+
+
+class markerCustomClass
+{
+    
+    var lat : CGFloat!
+    var long : CGFloat!
+    var title : NSString!
+    var snippet : NSString!
+    var image : UIImage!
+    
+    
+    init(lat:CGFloat,withLong long:CGFloat,withtitle title:NSString,withsnippet snippet:NSString,withimage image:UIImage)
+    {
+        self.lat = lat
+        self.long = long
+        self.title = title
+        self.snippet = snippet
+        self.image = image
+    }
+}
+
+class MapViewController: UIViewController ,GMSMapViewDelegate {
+    
+    var arrData = [markerCustomClass]()
+    
     
     
     
@@ -21,19 +46,97 @@ class MapViewController: UIViewController {
 
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: 36.318466,  longitude: 74.649515, zoom: 1.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        let camera = GMSCameraPosition.camera(withLatitude: 30.3753,  longitude: 69.3451, zoom: 8)
         
-        mymapView = mapView
+        mymapView.mapType = kGMSTypeNormal
+        mymapView.camera = camera
+        mymapView.delegate = self
+        
+        
+        
+        arrData.append(markerCustomClass.init(lat: 30.3753, withLong: 69.3451, withtitle: "Pakistan", withsnippet: "APna Pakistan", withimage: #imageLiteral(resourceName: "map-marker")))
+        
+        arrData.append(markerCustomClass.init(lat: 30.3653, withLong: 69.3451, withtitle: "Pakistan 2", withsnippet: "APna Pakistan", withimage: #imageLiteral(resourceName: "map_marker_blue")))
+        
+        arrData.append(markerCustomClass.init(lat: 30.3553, withLong: 69.3451, withtitle: "Pakistan 3", withsnippet: "APna Pakistan", withimage: #imageLiteral(resourceName: "map-marker")))
+        
+        arrData.append(markerCustomClass.init(lat: 30.3453, withLong: 69.3451, withtitle: "Pakistan 4", withsnippet: "APna Pakistan", withimage: #imageLiteral(resourceName: "map_marker_blue")))
+
+        
+        
+        
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        
+        for markerCustomObj in arrData {
+            
+            self.addMarkerWithCustomMarkerObj(customMarker: markerCustomObj)
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
+    func addMarkerWithCustomMarkerObj(customMarker : markerCustomClass)
+    {
+        //  Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: CLLocationDegrees(customMarker.lat), longitude: CLLocationDegrees(customMarker.long))
+        marker.title = customMarker.title as String?
+        marker.snippet = customMarker.snippet as String?
+        marker.icon = customMarker.image
+        
+        marker.map = mymapView
+    }
+    
+    
+    func mapView(_ mapView: GMSMapView, didLongPressAt coordinate: CLLocationCoordinate2D) {
+        
+        let marker3 : GMSMarker = GMSMarker.init()
+        marker3.position = coordinate
+        marker3.title = "170 Asbury Anderson Rd"
+        marker3.snippet = "US"
+        marker3.map = mymapView;
+        
+        let alert = UIAlertController(title: "Alert", message: "Message", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { action in
+             print("Segue yaha ayea ga")
+            // Segue yaha ayea ga
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { action in
+            print("yaha kuch nahi ayea ga")
+             // yaha kuch nahi ayea ga
+            
+        }))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    
+//    -(void) mapView:(GMSMapView *)mapView didLongPressAtCoordinate:(CLLocationCoordinate2D)coordinate{
+//    GMSMarker *marker3 = [[GMSMarker alloc] init];
+//    marker3.position = coordinate;
+//    marker3.title = @"170 Asbury Anderson Rd";
+//    marker3.snippet = @"US";
+//    marker3.map = mapView_;
+//    }
+    
+    
+//    - (void)mapView:(GMSMapView *)mapView didTapInfoWindowOfMarker:(GMSMarker *)marker {
+//    // your code
+//    }
+
+    
+    
     /*
     // MARK: - Navigation
 
